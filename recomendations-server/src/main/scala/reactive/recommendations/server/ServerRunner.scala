7a75027dddd91d8e3d0d2960552f68c688b6e4ec
Dzianis Balyka <d_balyka@wargaming.net>
2014-11-19 18:18:42 +0300

    server template

---
+++ recomendations-server/src/main/scala/reactive/recommendations/server/ServerRunner.scala
@@ -0,0 +1,54 @@
+package reactive.recommendations.server
+
+import _root_.akka.actor.ActorSystem
+import _root_.akka.io.IO
+import _root_.akka.util.Timeout
+import org.slf4j.LoggerFactory
+import reactive.recommendations.server.akka.ServiceUI
+import scopt.OptionParser
+import spray.can.Http
+import _root_.akka.pattern.ask
+
+import scala.concurrent.duration._
+
+/**
+ * Created by d_balyka on 19.11.2014.
+ */
+object ServerRunner {
+
+  val log = LoggerFactory.getLogger(ServerRunner.getClass)
+
+  val parser = new OptionParser[ServerConfig](ServerRunner.getClass.getName) {
+    head("Runs recomendations server")
+    help("help") text ("prints this usage text")
+    opt[String]('h', "host") action {
+      (conf, c) =>
+        c.copy(host = conf)
+    } text ("host to bind")
+    opt[Int]('p', "port") action {
+      (conf, c) =>
+        c.copy(port = conf)
+    } text ("port to bind")
+  }
+
+  def main(args: Array[String]): Unit = {
+    parser.parse(args, ServerConfig()) map {
+      config =>
+        log.info("processing with {}", config)
+
+        //start akka with spray
+        implicit val system = ActorSystem("RecommendatioSystem")
+        val serviceUi = system.actorOf(ServiceUI.props(), "serviceUi")
+
+        implicit val timeout = Timeout(5.seconds)
+
+        IO(Http) ? Http.Bind(serviceUi, interface = config.host, port = config.port)
+
+    } getOrElse {
+      // arguments are bad, error message will have been displayed
+    }
+  }
+
+}
+
+case class ServerConfig(host: String = "localhost", port: Int = 8080)
---
+++ recomendations-server/src/main/scala/reactive/recommendations/server/akka/ServiceUI.scala
@@ -0,0 +1,55 @@
+package reactive.recommendations.server.akka
+
+import akka.actor.{Props, Actor}
+import spray.http.{StatusCodes, StatusCode, MediaTypes}
+import spray.routing.HttpService
+
+/**
+ * Created by d_balyka on 19.11.2014.
+ */
+object ServiceUI {
+  def props(): Props = Props(new ServiceUI())
+}
+
+class ServiceUI extends Actor with Service {
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(managingRoute)
+}
+
+
+trait Service extends HttpService {
+  val managingRoute =
+    path("recommend") {
+      get {
+        respondWithMediaType(MediaTypes.`application/json`) {
+          complete {
+            "recommend"
+          }
+        }
+      }
+    }
+  path("itemViewed") {
+    post {
+      respondWithStatus(StatusCode.Success) {
+      }
+    }
+  }
+  path("user") {
+    post {
+      respondWithStatus(StatusCodes.Success) {
+      }
+    }
+  }
+  path("item") {
+    post {
+      respondWithStatus(StatusCodes.Success) {
+      }
+    }
+  }
+}
