2894b50e8b1f9ed00c5262eef2015669365d8a86
Dzianis Balyka <d_balyka@wargaming.net>
2014-11-27 17:33:56 +0300

    json marshaller

--- recomendations-server/src/main/scala/reactive/recommendations/server/akka/ServiceUI.scala
+++ recomendations-server/src/main/scala/reactive/recommendations/server/akka/ServiceUI.scala
@@ -1,7 +1,13 @@
 package reactive.recommendations.server.akka
 
 import akka.actor.{Props, Actor}
-import spray.http.{StatusCodes, StatusCode, MediaTypes}
+import spray.http.{MediaTypes}
+import spray.json.DefaultJsonProtocol
+import spray.json.DefaultJsonProtocol._
+import spray.httpx.SprayJsonSupport
+import spray.httpx.SprayJsonSupport._
+
+
 import spray.routing.HttpService
 
 /**
@@ -23,42 +29,55 @@ class ServiceUI extends Actor with Service {
 }
 
 
+case class Recommendation(user: String, items: Array[String])
+
+case class Action(user: String, item: String, action: String, params: Map[String, String])
+
+case class Item(id: String, tags: Set[String], categories: Set[String])
+
+case class User(id: String)
+
+
 trait Service extends HttpService {
+
   val managingRoute =
     path("recommend") {
       get {
         respondWithMediaType(MediaTypes.`application/json`) {
           complete {
-            "recommend"
+            implicit val reco = jsonFormat2(Recommendation)
+            Recommendation("sss", Array("i1", "i2"))
           }
         }
       }
-    }
-  path("itemViewed") {
-    post {
-      respondWithStatus(200) {
-        complete{
-          ""
+    } ~
+      path("action") {
+        (get | post) {
+          respondWithStatus(200) {
+            complete {
+              "hi!!!!!!!!!!"
+            }
+          }
+        }
+      } ~
+      path("user") {
+        post {
+          respondWithStatus(200) {
+            complete {
+              "user"
+            }
+          }
+        }
+      } ~
+      path("item") {
+        post {
+          respondWithStatus(200) {
+            complete {
+              "item"
+            }
+          }
         }
       }
-    }
-  }
-  path("user") {
-    post {
-      respondWithStatus(200) {
-        complete{
-          ""
-        }
-      }
-    }
-  }
-  path("item") {
-    post {
-      respondWithStatus(200) {
-        complete{
-          ""
-        }
-      }
-    }
-  }
 }
+
+
